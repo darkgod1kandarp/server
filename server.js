1f5d90245b46dc1493756058012f664c1013a88d
darkgod1kandarp <009kandarp@gmail.com>
2021-07-07 10:47:48 +0530

    op

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+app.post('/api/posts', verifyToken, (req, res) => {  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  console.log(123);
+  // Mock user
+  const user = {
+    id: 1, 
+    username: 'brad',
+    email: 'brad@gmail.com'
+  }
+
+  jwt.sign({user}, 'secretkey', (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+app.post('/api/datagaining',verifyToken,(req,res)=>{
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err){
+      res.sendStatus(403);
+    }
+    else{
+      res.json(req.query)
+     
+    }
+  })
+})
+
+
+
+function verifyToken(req, res, next) {
+ 
+  const bearerHeader = req.headers['authorization'];
+  console.log(bearerHeader);
+ 
+  if(typeof bearerHeader !== 'undefined') {
+  
+    const bearer = bearerHeader.split(' ');
+
+    const bearerToken = bearer[1];
+    
+    req.token = bearerToken;
+ 
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+
+}
+
+app.listen(5000, () => console.log('Server started on port 5000'));
