41cb2c36b393fc99309c85f5510d386f1488c2df
darkgod1kandarp <009kandarp@gmail.com>
2021-08-27 16:48:29 +0530

    pushing

--- server.js
+++ server.js
@@ -74,36 +74,11 @@ const pool = mysql.createPool({
 const db = pool.promise();
 
 
-  cloudinary.uploader.upload(dataURI, (err, result) => {
-    console.log(err);
-    if (err) return err;
-    return result.url;
-  });
+ 
 
 
-app.get("/jwttoken", (req, res) => {
-  // Mock user
 
-  jwt.sign({}, "secretkey", (err, token) => {
-    res.json({
-      token,
-    });
-  });
-});
 
-app.post("/signup", async (req, res) => {
-  const { username, email, password } = req.body;
-  const uuid1 = makeid(11);
-  let sql = `  select checking1('${uuid1}','${username}','${email}','${password}') as c1;`;
-
-  const [row1] = await db.query(sql);
-
-  if (row1[0].c1 === 1) {
-    res.json({ message: "alva" });
-  } else {
-    res.json({ message: "nalva", data: `${uuid1}` });
-  }
-});
 let id;
 io.on("connection", async(socket) => {
       id =  socket.id;
@@ -113,8 +88,7 @@ io.on("connection", async(socket) => {
   socket.on("connected", async(data) => {
    
     const{userid} = data;
-    
-    
+
      id =  socket.id;
       let sql  =  `update socketid set userid1 = '${userid}' where ids = '${id}';`
       await db.query(sql);
@@ -122,8 +96,16 @@ io.on("connection", async(socket) => {
 
   socket.on("post",async(data)=>{
     const{userid,image,text_des,title}=data;
+    let url;
+    
+    cloudinary.uploader.upload(image, async(err, result) => {
+    if (err) return err;
+    const url = result.url;
+    sql =`insert into post values('${userid}','${title}','${text_des}','${url}')`
+    await db.query(sql); 
+    });
     const dataSending ={image,text_des,title}
-    let sql =`select * from  connection_people where connector = '${userid}' or connecting = '${userid}';`
+    let sql =`select * from  connection_people where connector = '${userid}' or connecting = '${userid}' and sided = 1;`
     const[row1,column1] =  await db.query(sql);
     console.log(row1);
     var flipFlop = []
@@ -142,17 +124,43 @@ io.on("connection", async(socket) => {
       io.to(x.ids).emit("sendingData",dataSending)
     }
 
-
-
+   
     
-    
-    // let sql = `select * from socketid where userid1 = '${userid}'; `
-    // const [row1,column1] =  await db.query(sql);
-    // console.log(row1)
-    // for (let x of row1){
-    //   io.to(x.ids).emit("data",data);
+  })
 
-    // }
+socket.on("connectingUser",async(data)=>{
+   const {connecting,connector} = data;
+   console.log(data)
+   let sql = `select count(*) as c1 from connection_people where (connector = '${connector}'and connecting = '${connecting}') or (connector = '${connecting}'and connecting = '${connector}');`
+   const [counting,column4] =  await db.query(sql);
+   console.log(counting);
+   if (counting[0].c1===0){
+   sql =`insert into connection_people values('${connector}','${connecting}',0);`
+   await db.query(sql);
+   sql = `select * from socketid where userid1 = '${connecting}';`
+   const[row1,column1] = await db.query(sql);
+   sql =`select username from useinfo where userid1 = '${connector}';`
+   const[row2,column2] = await db.query(sql);
+   const sendingData = {name:row2[0].userid1,msg:"ftm"}
+   
+   for(let x of row1){
+    io.to(x.ids).emit("sendingData",sendingData)
+   }
+  }
+  else{
+    sql = `update connection_people set sided = 1 where connecting = '${connector}' and connector = '${connecting}';`
+    await db.query(sql)
+    sql = `select * from socketid where userid1 = '${connecting}';`
+   const[row1,column1] = await db.query(sql);
+   sql = `select username from userinfo where userid1 = '${connector}';`
+   const[row2,column2] = await db.query(sql);
+   const sendingData = {name:row2[0].userid1,msg:"stm"}
+   
+   for(let x of row1){
+    io.to(x.ids).emit("sendingData",sendingData)
+   }
+  }
+
   })
 
 socket.on("disconnect",async(data)=>{
@@ -163,189 +171,3 @@ socket.on("disconnect",async(data)=>{
   })
 
 });
-app.post('/userprofile',async(req,res)=>{
-  const {userid} = req.body;
-  let sql = `select username,email,type_account,university_name,location from userinfo,user_account_details where userinfo.userid1 = '${userid}' and user_account_details.userid1='${userid}'  ;`
-  console.log(sql)
-  const [row1,column1] = await db.query(sql);
-  console.log(row1)
-  return res.send(row1)
-})
-// toshi@explified.com
-//kushal@explified.com
-app.post("/profile", async (req, res) => {
-  const { userid, typeofaccount, location, collegeName, userimage } = req.body;
-  let sql;
-  
-  cloudinary.uploader.upload(userimage, async(err, result) => {
-    if (err) return err;
-    sql = `insert into user_account_details value('${userid}','${typeofaccount}','${location}','${collegeName}','${result.url}');`;
-  await db.query(sql);
-    
-  });
-
-  
-  if (typeofaccount === "student") {
-    const { startYear, lastYear, skills_already, skills_demanded } = req.body;
-    sql = `insert into student_account values('${userid}','${startYear}','${lastYear}');`;
-    await db.query(sql);
-    sql = `insert into skills_already_have values `;
-    var x;
-    d = [];
-    for (x of skills_already) {
-      d.push(`('${userid}','${x}')`);
-    }
-    sql += d.join(",");
-  
-    await db.query(sql);
-    sql = `insert into skills_demand values `;
-    var x;
-    d = [];
-    for (x of skills_demanded) {
-      d.push(`('${userid}','${x}')`);
-    }
-    sql += d.join(",");
-    await db.query(sql);
-  } else if (typeofaccount === "subjme") {
-    const { subject, experience, coursetosell, jobrequired } = req.body;
-    sql = `insert into student_account values('${userid}','0','${experience}');`;
-    await db.query(sql);
-    sql = `insert into skills_already_have values `;
-    var x;
-    d = [];
-    for (x of subject) {
-      d.push(`('${userid}','${x}')`);
-    }
-    sql += d.join(",");
-    await db.query(sql);
-    sql = `insert into skills_demand values `;
-    var x;
-    d = [];
-    for (x of coursetosell) {
-      d.push(`('${userid}','${x}')`);
-    }
-    sql += d.join(",");
-
-    await db.query(sql);
-    d = [];
-    sql = `insert into jobs_required values `;
-    for (x of jobrequired) {
-      d.push(`('${userid}','${x}')`);
-    }
-    sql += d.join(",");
-
-    await db.query(sql);
-  } else {
-    const { jobInstance } = req.body;
-    sql = `insert into jobs values`;
-    d = [];
-    sql = `insert into jobs_taken_account values `;
-    const uuid2 = makeid(11);
-    console.log(uuid2, 123);
-    for (x of jobInstance) {
-      d.push(
-        `('${userid}','${x.uuid1}','${x.job}','${x.startSalary}','${x.endSalary}')`
-      );
-    }
-
-    sql += d.join(",");
-    sql = `insert into jobs_language values `;
-    d = [];
-    let p;
-    for (x of jobInstance) {
-      for (p of x.language) {
-        d.push(`('${x.uuid1}','${p}')`);
-      }
-    }
-    sql += d.join(",");
-    console.log(sql);
-    await db.query(sql);
-  }
-  return res.send({ data: "send" });
-});
-
-app.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  let sql = `select count(*) as c1 from userinfo where email = '${email}' and password = '${password}';`;
-  console.log(sql);
-  const [row1, column1] = await db.query(sql);
-  console.log(row1[0].c1);
-  if (row1[0].c1 === 0) {
-    res.json({ message: "nuf" });
-  } else {
-    res.json({
-      message: "uf",
-    });
-  }
-});
-
-app.post("/checking", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", async (err, authData) => {
-    if (err) {
-      //  console.log(err)
-      res.json({ message: "not verified" });
-    } else {
-      res.json({ message: "verified" });
-    }
-  });
-});
-
-app.post("/forgotpassword", async (req, res) => {
-  const { email } = req.body;
-  let sql = `select count(*) as c1 from userinfo where email = '${email}';`;
-  const [row1, column1] = await db.query(sql);
-  if (row1[0].c1 == 1) {
-    const opo = otp();
-    mailjet
-      .post("send", { version: "v3.1" })
-      .request({
-        Messages: [
-          {
-            From: {
-              Email: "rushabh.s1@ahduni.edu.in",
-              Name: "carvan",
-            },
-            To: [
-              {
-                Email: email,
-              },
-            ],
-            Subject: "OTP123",
-            TextPart: `your otp is ${opo}`,
-          },
-        ],
-      })
-      .then((result) => {
-        res.json({ data: opo });
-      })
-      .catch((err) => {
-        res.json({ data: "uu" });
-      });
-  } else {
-    res.json({ data: "no userfound od this type" });
-  }
-});
-
-app.post("/changepassword", async (req, res) => {
-  const { email, password } = req.body;
-  let sql = `update userinfo set password ='${password}' where email = '${email}';`;
-  await db.query(sql);
-  res.send({ message: "updated" });
-});
-
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
-
-  if (typeof bearerHeader !== "undefined") {
-    const bearer = bearerHeader.split(" ");
-
-    const bearerToken = bearer[1];
-
-    req.token = bearerToken;
-
-    next();
-  } else {
-    // Forbidden
-    res.sendStatus(403);
-  }
-}
